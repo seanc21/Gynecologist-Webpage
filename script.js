ffea9344fca89cf3b1a70014c54e943fe5ad5bfc
Sean <72217270+seanc21@users.noreply.github.com>
2021-10-29 00:45:22 -0500

    mobile version complete

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const header = document.querySelector("header");
 const sectionOne = document.querySelector("#showcase");
+const parallax = document.querySelector("#mobile-parallax");
 
 const sectionOneOptions = {
   rootMargin: "-700px 0px 0px 0px"
@@ -12,6 +13,7 @@ const sectionOneObserver = new IntersectionObserver(function(
   entries.forEach(entry => {
     if (!entry.isIntersecting) {
       header.classList.add("nav-scrolled");
+      parallax.classList.add("add-parallax");
     } else {
       header.classList.remove("nav-scrolled");
     }
